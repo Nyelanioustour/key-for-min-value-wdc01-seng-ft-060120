6237859c8337ddee66f5302be663d5b61fdbefd7
Shawn Alexander <shawn.k.alexander@gmail.com>
2020-05-25 17:05:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+   
+name_hash.each do |key, value|
+  comparison = value 
+end 
+name_hash.each do |key, value|
+  if value < comparison
+    comparison = value
+  end
+  name_hash.each |key, value|
+  if value == comparison
+    key
+  end
 end
