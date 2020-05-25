5b3b82c8685aa6d85e47f282d88e1e9c27fa5277
Shawn Alexander <shawn.k.alexander@gmail.com>
2020-05-25 17:25:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,14 @@
 require 'pry'
 def key_for_min_value(name_hash)
    comparison = 999999999 
+   output = ""
 name_hash.each do |key, value|
   if value < comparison 
+    comparison = value
     output = key
   end
-  p output
 end
-  
+  binding.pry 
   
 
   
