7a613903204d485d86607821b5dccea19346b5cf
Shawn Alexander <shawn.k.alexander@gmail.com>
2020-05-25 17:30:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,9 @@ def key_for_min_value(name_hash)
    comparison = 999999999 
    output = ""
 name_hash.each do |key, value|
-  if 
-  else value < comparison 
+  if name_hash == {}
+    nil 
+  elsif value < comparison 
     comparison = value
     output = key
   end
